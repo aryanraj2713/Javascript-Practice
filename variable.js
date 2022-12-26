5133f2d51106f23768113ccd6cc979132d40328c
Aryan Raj <aryanraj2713@gmail.com>
2022-12-27 00:12:45 +0530

    basic

---
+++ variable.js
@@ -0,0 +1,22 @@
+let age = 25 ;
+let year = 2025 ;
+
+console.log(age,year);
+
+age = 30 ;
+
+console.log(age);
+
+//<------------------------------------------------>
+
+const point = 40 ;
+
+//point = 50 ;     Not allowed
+
+
+//old method
+
+var score = 75 ;
+
+console.log(score);
+
